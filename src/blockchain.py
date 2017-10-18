fcfdea82bad626ccdf3624f076b227895422fa10
wizh <mathias@fleig.io>
2017-10-18 08:53:16 -0700

    Add blockchain skeleton

---
+++ src/blockchain.py
@@ -0,0 +1,15 @@
+class Blockchain():
+    def __init__(self):
+        pass
+
+    def add_block(self, block):
+        pass
+
+    def get_latest_block(self):
+        pass
+
+    def get_block(self, index):
+        pass
+
+    def create_genesis_block(self):
+        pass
